91c9324462ba061cf3ae0f59d96ceb4d9e9d9a3a
Westender <javaxwestender@gmail.com>
2023-07-26 20:04:29 +0300

    Add: task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
 
+    static int counter = 1;
+
+    public static void main(String[] args) {
+        task1();
     }
+
+    static void task1() {
+        System.out.println("\nЗадание " + counter++ + ":");
+        int money = 15000;
+        int sum = 0;
+        int month = 0;
+
+        while (sum < 2_459_000) {
+            sum += money;
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+        }
+    }
+
 }

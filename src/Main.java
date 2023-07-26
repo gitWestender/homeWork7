9630e4f2c58c48742db902f3c724b7b1227681bc
Westender <javaxwestender@gmail.com>
2023-07-26 20:36:42 +0300

    Add: task2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     static void task1() {
         System.out.println("\nЗадание " + counter++ + ":");
+
         int money = 15000;
         int sum = 0;
         int month = 0;
@@ -16,6 +18,23 @@ public class Main {
             sum += money;
             month++;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
+        }
+    }
+
+    static void task2() {
+        System.out.println("\nЗадание " + counter++ + ":");
+
+        int minNum = 1;
+        int maxNum = 10;
+
+        while (minNum <= maxNum) {
+            System.out.print(minNum + " ");
+            minNum++;
+        }
+
+        System.out.println();
+        for (int i = maxNum; i > 0; i--) {
+            System.out.print(i + " ");
         }
     }
 
